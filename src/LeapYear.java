fc6d5da9f8659082e034873c5473ed1408991d90
AparnaBsutar <apol271115@gmail.com>
2025-06-05 13:38:36 +0530

    demo

---
+++ src/LeapYear.java
@@ -0,0 +1,12 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2025;
+        if ((year % 4 == 0) ||  (year % 400 == 0) ) {
+            System.out.println(year + " leap year");
+        } else {
+            System.out.println(year + " year is not leap");
+        }
+
+
+    }
+}
