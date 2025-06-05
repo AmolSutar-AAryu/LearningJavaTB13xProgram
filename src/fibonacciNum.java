d0cb5b3c7a128c3af704fdf0e3eeb032309ed067
AparnaBsutar <apol271115@gmail.com>
2025-06-05 15:39:34 +0530

    program for Fibonacci Number

---
+++ src/fibonacciNum.java
@@ -0,0 +1,15 @@
+public class fibonacciNum {
+    public static void main(String[] args) {
+
+        int n =10,  a= 0, b = 1;
+        System.out.println("fibonacci series: ");
+        for(int  i = 0; i<n; i++){
+            System.out.println(a+" ");
+            int sum =a+b;
+            a=b;
+            b=sum;
+
+
+        }
+    }
+}
