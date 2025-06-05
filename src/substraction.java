ea7164a599ef38a6c0d83cbbb5d62526b437fe5b
AparnaBsutar <apol271115@gmail.com>
2025-06-05 12:38:08 +0530

    demo

---
+++ src/substraction.java
@@ -0,0 +1,7 @@
+public class substraction {
+    public static void main(String[] args) {
+        int a = 30; int b = 40;
+        int sub = b-a;
+        System.out.println("substraction is" + sub );
+    }
+}
