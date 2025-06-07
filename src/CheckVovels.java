b6ad0a7d889cc69478731b45a68f0850c51073df
Amol_Sutar <amol.sutar677@gmail.com>
2025-06-07 23:45:17 +0530

    programs for finding vowel and swaping the two numbers

---
+++ src/CheckVovels.java
@@ -0,0 +1,13 @@
+public class CheckVovels {
+    public static void main(String[] args) {
+
+        char ch = 'b';
+
+        if (ch == 'e' || ch == 'a' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println(ch+" is a vowel");
+        }else {
+            System.out.println(ch+" is a not vowel");
+        }
+    }
+    }
+
