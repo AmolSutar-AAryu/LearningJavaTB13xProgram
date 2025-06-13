585813409800209cf71c11977e9854ab3f6bfe04
Amol_Sutar <amol.sutar677@gmail.com>
2025-06-13 13:22:52 +0530

    Programs for increment and decrement operator in java

---
+++ src/ex_07_Increment_Decrement_OP/Lab069_In_de_op.java
@@ -0,0 +1,8 @@
+package ex_07_Increment_Decrement_OP;
+
+public class Lab069_In_de_op {
+    public static void main(String[] args) {
+        String string_args = args[0];
+        System.out.println(string_args);
+    }
+}
---
+++ src/ex_07_Increment_Decrement_OP/Lab071_POST_ID.java
@@ -0,0 +1,13 @@
+package ex_07_Increment_Decrement_OP;
+
+public class Lab071_POST_ID {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(++a);
+        System.out.println(a++);
+
+        int a_post = 20;
+        System.out.println(a_post++);
+        System.out.println(a_post);
+    }
+}
