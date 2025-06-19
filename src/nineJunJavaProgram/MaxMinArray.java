1e70a2ae075dd02b6a8ea0190995e0bba7e29ea4
Amol Sutar <amol.sutar677@gmail.com>
2025-06-19 07:25:13 -0700

    MaxMinArray program added

---
+++ src/nineJunJavaProgram/MaxMinArray.java
@@ -0,0 +1,16 @@
+package nineJunJavaProgram;
+
+public class MaxMinArray {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 20, 15};
+        int max = arr[0], min = arr[0];
+        for (int num : arr) {
+            if (num > max) max = num;
+            if (num < min) min = num;
+        }
+        System.out.println("Max: " + max + ", Min: " + min);
+    }
+
+    }
+
+
