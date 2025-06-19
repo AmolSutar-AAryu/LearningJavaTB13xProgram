dc6bb0c8a29891d0604ec072925a91342ef1095b
Amol Sutar <amol.sutar677@gmail.com>
2025-06-19 07:29:25 -0700

    MaxMinArray program added

--- src/nineJunJavaProgram/MaxMinArray.java
+++ src/nineJunJavaProgram/MaxMinArray.java
@@ -7,6 +7,8 @@ public class MaxMinArray {
         for (int num : arr) {
             if (num > max) max = num;
             if (num < min) min = num;
+
+
         }
         System.out.println("Max: " + max + ", Min: " + min);
     }
