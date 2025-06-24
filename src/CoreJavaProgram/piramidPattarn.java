d1573c50dfddc33fd5bd42a59ee36d499733e239
Amol Sutar <amol.sutar677@gmail.com>
2025-06-24 23:31:53 +0530

    Programs for pyramid pattarn

---
+++ src/CoreJavaProgram/piramidPattarn.java
@@ -0,0 +1,18 @@
+package CoreJavaProgram;
+
+public class piramidPattarn
+{
+    public static void main(String[] args) {
+        int size =5;
+        for(int i = 1; i<=size ; i++){
+            for(int j =i; j<size; j++ ){
+                System.out.print(" ");
+            }
+            for(int j =1; j<=(2*i-1); j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
+}
