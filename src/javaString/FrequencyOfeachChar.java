5b8ae9608cfe2b1ab61429eb9571c73360df64dc
Amol Sutar <amol.sutar677@gmail.com>
2025-06-26 12:10:43 +0530

    String operation frequency of each charactor

---
+++ src/javaString/FrequencyOfeachChar.java
@@ -0,0 +1,22 @@
+package javaString;
+
+public class FrequencyOfeachChar {
+    public static void main(String[] args) {
+        String input = "This is my name";
+
+        char target = 'i';
+
+        int count = 0;
+
+        input = input.toLowerCase();
+
+        for (char ch : input.toCharArray()) {
+
+            if (ch==target)
+            {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
