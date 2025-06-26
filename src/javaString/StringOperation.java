e6a319d0888737cbcc238a7c9b4a580866cd3b08
Amol Sutar <amol.sutar677@gmail.com>
2025-06-26 08:05:27 +0530

    String operation

---
+++ src/javaString/StringOperation.java
@@ -0,0 +1,33 @@
+package javaString;
+
+public class StringOperation {
+    public static void main(String[] args) {
+
+        String str = "amolsutar";
+        String str1 = "aaryan";
+
+        String str3 = str.concat(str1);
+        System.out.println(str3);
+
+        System.out.println(str.length());
+        System.out.println(str.substring(2, 3));
+
+        String str4 = "Test";
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println("Character at index " + i + ": " + str4.charAt(i));
+        }
+
+        String s1 = "Hello";
+        String s2 = "hello";
+        String s3 = "hello";
+
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s3));
+        System.out.println(s1.equalsIgnoreCase(s3));
+        System.out.println(s2.compareTo(s3));
+
+
+    }
+}
+
